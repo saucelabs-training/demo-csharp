5b29296bce817249a288a31fb0b2643fc3bfd771
nadvolod <nadvolod@gmail.com>
2023-06-29 19:06:54 +0000

    working web test

--- Dotnet/SeleniumWeb.cs
+++ Dotnet/SeleniumWeb.cs
@@ -6,66 +6,69 @@ using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Remote;
 using OpenQA.Selenium.Support.UI;
 
-[TestClass]
-public class SeleniumWeb
+namespace Dotnet
 {
-    [TestMethod]
-    public void SimpleTest()
+    [TestClass]
+    public class SeleniumWeb
     {
-        Assert.IsTrue(true);
-    }
-    public IWebDriver Driver { get; set; }
+        [TestMethod]
+        public void SimpleTest()
+        {
+            Assert.IsTrue(true);
+        }
+        public IWebDriver Driver { get; set; }
 
-    public TestContext TestContext { get; set; }
+        public TestContext TestContext { get; set; }
 
-    [TestInitialize]
-    public void Setup()
-    {
-        var browserOptions = new ChromeOptions();
-        browserOptions.PlatformName = "Windows 10";
-        browserOptions.BrowserVersion = "latest";
+        [TestInitialize]
+        public void Setup()
+        {
+            var browserOptions = new ChromeOptions();
+            browserOptions.PlatformName = "Windows 10";
+            browserOptions.BrowserVersion = "latest";
 
-        var sauceOptions = new Dictionary<string, object>();
-        sauceOptions.Add("name", TestContext.TestName);
-        sauceOptions.Add("username", Environment.GetEnvironmentVariable("SAUCE_USERNAME"));
-        sauceOptions.Add("accessKey", Environment.GetEnvironmentVariable("SAUCE_ACCESS_KEY"));
+            var sauceOptions = new Dictionary<string, object>();
+            sauceOptions.Add("name", TestContext.TestName);
+            sauceOptions.Add("username", Environment.GetEnvironmentVariable("SAUCE_USERNAME"));
+            sauceOptions.Add("accessKey", Environment.GetEnvironmentVariable("SAUCE_ACCESS_KEY"));
 
-        browserOptions.AddAdditionalOption("sauce:options", sauceOptions);
-        var sauceUrl = new Uri("https://ondemand.us-west-1.saucelabs.com/wd/hub");
+            browserOptions.AddAdditionalOption("sauce:options", sauceOptions);
+            var sauceUrl = new Uri("https://ondemand.us-west-1.saucelabs.com/wd/hub");
 
-        Driver = new RemoteWebDriver(sauceUrl, browserOptions);
-    }
+            Driver = new RemoteWebDriver(sauceUrl, browserOptions);
+        }
 
-    [TestMethod]
-    public void LoginTest()
-    {
-        Driver.Navigate().GoToUrl("https://www.saucedemo.com");
+        [TestMethod]
+        public void LoginTest()
+        {
+            Driver.Navigate().GoToUrl("https://www.saucedemo.com");
 
-        var usernameLocator = By.CssSelector("#user-name");
-        var passwordLocator = By.CssSelector("#password");
-        var submitLocator = By.CssSelector(".btn_action");
+            var usernameLocator = By.CssSelector("#user-name");
+            var passwordLocator = By.CssSelector("#password");
+            var submitLocator = By.CssSelector(".btn_action");
 
-        var wait = new WebDriverWait(Driver, TimeSpan.FromSeconds(15));
-        wait.Until(drv => drv.FindElement(usernameLocator));
+            var wait = new WebDriverWait(Driver, TimeSpan.FromSeconds(15));
+            wait.Until(drv => drv.FindElement(usernameLocator));
 
-        var usernameElement = Driver.FindElement(usernameLocator);
-        var passwordElement = Driver.FindElement(passwordLocator);
-        var submitElement = Driver.FindElement(submitLocator);
+            var usernameElement = Driver.FindElement(usernameLocator);
+            var passwordElement = Driver.FindElement(passwordLocator);
+            var submitElement = Driver.FindElement(submitLocator);
 
-        usernameElement.SendKeys("standard_user");
-        passwordElement.SendKeys("secret_sauce");
-        submitElement.Click();
+            usernameElement.SendKeys("standard_user");
+            passwordElement.SendKeys("secret_sauce");
+            submitElement.Click();
 
-        Assert.AreEqual("https://www.saucedemo.com/inventory.html", Driver.Url);
-    }
+            Assert.AreEqual("https://www.saucedemo.com/inventory.html", Driver.Url);
+        }
 
-    [TestCleanup]
-    public void Teardown()
-    {
-        var isPassed = TestContext.CurrentTestOutcome == UnitTestOutcome.Passed;
-        var script = "sauce:job-result=" + (isPassed ? "passed" : "failed");
-        ((IJavaScriptExecutor)Driver).ExecuteScript(script);
+        [TestCleanup]
+        public void Teardown()
+        {
+            var isPassed = TestContext.CurrentTestOutcome == UnitTestOutcome.Passed;
+            var script = "sauce:job-result=" + (isPassed ? "passed" : "failed");
+            ((IJavaScriptExecutor)Driver).ExecuteScript(script);
 
-        Driver?.Quit();
+            Driver?.Quit();
+        }
     }
 }
